7ea6dbff5d011ba4737966d8084b39c61b93e60f
ajmalch <ajmalch@gmail.com>
2017-05-10 22:20:55 -0400

    Removed WebHook From the App

--- src/main/java/com/example/controller/PersonRestController.java
+++ src/main/java/com/example/controller/PersonRestController.java
@@ -37,21 +37,4 @@ public class PersonRestController {
     public Person updatePerson(@RequestParam String firstname, @PathVariable String lastname){
        return personService.updatePerson(firstname,lastname);
     }
-
-    @PostMapping(path="/webhook")
-    public String webhookCallBack(@RequestBody String input){
-        return null;
-    }
-
-    @GetMapping(path="/webhook")
-    public String webhookGetCallBack(
-            @RequestParam("hub.mode") String hubMode,
-            @RequestParam("hub.challenge") String hubChallenge,
-            @RequestParam("hub.verify_token") String hubVerifyToken){
-
-        if(hubVerifyToken.equals("ajmal_fb_webhook"))
-            return hubChallenge;
-        else
-            return null;
-    }
 }
