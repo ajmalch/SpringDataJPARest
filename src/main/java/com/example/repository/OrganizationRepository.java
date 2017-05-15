07b6f7e49d5d79d21d04dd3f69fe3a8f8eb00c65
ajmalch <ajmalch@gmail.com>
2017-05-14 23:08:06 -0400

    Added Some Profile specific properties and made DELETE not possible on Organizattion REST resource

--- src/main/java/com/example/repository/OrganizationRepository.java
+++ src/main/java/com/example/repository/OrganizationRepository.java
@@ -2,6 +2,7 @@ package com.example.repository;
 
 import com.example.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Created by AjmalCholassery on 3/25/17.
@@ -9,4 +10,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrganizationRepository extends JpaRepository<Organization,Long> {
    <T> T findByShortname(String shortname, Class<T> projection);
+
+   @RestResource(exported = false)
+   @Override
+   void delete(Organization organization);
 }
